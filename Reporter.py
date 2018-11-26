3328d3bd6196c71f7492da20b4e8e14caa9b06b4
Onur <onurovalii@gmail.com>
2018-11-26 22:31:14 +0300

    v3

--- Reporter.py
+++ Reporter.py
@@ -1,3 +1,30 @@
-LASTXDAYS = 7
+import datetime
+import timedelta
+import GA
 
-print(LASTXDAYS)
+def reportDay(day):
+    # get analytics data for the day
+    # get adwords data for the day
+    # calculate roi for (in Google Ads class)
+    #   Dynamic Remarketing
+    #   SEM (Brand)
+    #   SEM (Non-Brand)
+    GA.main()
+
+
+LASTXDAYS = 1
+
+today = datetime.datetime.now().date()
+
+dayz = []
+i = 0
+while i < LASTXDAYS:
+    dayscount = i + 1
+    thatDay = today - timedelta.Timedelta( days = dayscount )
+    dayz.append(thatDay)
+    i = dayscount
+
+print(dayz)
+
+for d in dayz:
+    reportDay(d)
